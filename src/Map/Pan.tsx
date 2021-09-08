66b03446ee5505b7dc4861eb2daa0ba8cca30b8e
Hammerlord <nepharus@gmail.com>
2021-09-07 23:22:06 -0700

    Allow erasing nodes when drawing

--- src/Map/Pan.tsx
+++ src/Map/Pan.tsx
@@ -21,14 +21,32 @@ const Pan = ({ children }) => {
         setIsMouseDown(false);
         if (isPanning) {
             setIsPanning(false);
-        } else if (enableDraw) {
-            // This is just for generating routes
-            const { left, top } = containerRef.current.getBoundingClientRect();
-            const newPositions = [...positions, [e.clientX - left, e.clientY - top]];
+        }
+
+        handleDraw(e);
+    };
+
+    // This is just for generating routes
+    const handleDraw = (e) => {
+        if (!isPanning && enableDraw) {
+            const { left, width, top, height } = containerRef.current.getBoundingClientRect();
+            const newPositions = [...positions, [(e.clientX - left) / width, (e.clientY - top) / height]];
             console.log(newPositions);
             setPositions(newPositions);
         }
     };
+
+    const handleErase = (e, i) => {
+        if (!isPanning && enableDraw && e.button === 2) {
+            const newPositions = positions.slice();
+            newPositions.splice(i, 1);
+            setPositions(newPositions);
+            e.stopPropagation();
+            e.preventDefault();
+        }
+    };
+
+    const { width = 0, height = 0 } = (containerRef.current?.getBoundingClientRect && containerRef.current?.getBoundingClientRect()) || {};
 
     const handleMouseMove = (e) => {
         if (!isMouseDown) {
@@ -57,13 +75,14 @@ const Pan = ({ children }) => {
                 <div style={{ position: "absolute", left: 0, top: 0, width: `100%`, height: `100%` }}>
                     {positions.map(([x, y], i) => (
                         <div
+                            onClick={(e) => handleErase(e, i)}
                             key={i}
                             style={{
                                 position: "absolute",
                                 backgroundColor: "red",
                                 width: "2vh",
                                 height: "2vh",
-                                transform: `translate(${x}px, ${y}px)`,
+                                transform: `translate(${x * width}px, ${y * height}px)`,
                             }}
                         >
                             {i + 1}
