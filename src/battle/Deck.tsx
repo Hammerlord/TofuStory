7efb27dac247aad233608af3706c8b889bfe94dc
Hammerlord <nepharus@gmail.com>
2021-09-21 21:02:46 -0700

    Refactor deck colour

--- src/battle/Deck.tsx
+++ src/battle/Deck.tsx
@@ -1,7 +1,11 @@
+import { useEffect, useState } from "react";
 import { createUseStyles } from "react-jss";
 
-const ARSENAL_COLOR = "#176fbd";
+const DECK_COLOR = "#176fbd";
+const DECK_SHADOW = "#125896";
 const COOLDOWN_COLOR = "#aaaaaa";
+const COOLDOWN_SHADOW = "#8a8a8a";
+
 const useStyles = createUseStyles({
     root: {
         position: "relative",
@@ -29,64 +33,36 @@ const useStyles = createUseStyles({
 const Deck = ({ deck, discard }) => {
     const classes = useStyles();
 
-    // Holy cow, can we just change the colour based on the index?
-    const getShadow = (i: number) => {
-        if (i === 0) {
-            return null;
+    const getCardColor = (i: number): string => {
+        const isLast = i === deck.length + discard.length - 1;
+        if (i < discard.length) {
+            return isLast ? COOLDOWN_COLOR : COOLDOWN_SHADOW;
         }
-
-        return (
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                style={{
-                    bottom: `${i * 5 - 5}px`,
-                }}
-            >
-                <path
-                    fill={"rgba(0, 0, 0, 0.25)"}
-                    d="M 50 75 100 100 50 125 0 100 Z"
-                />
-            </svg>
-        );
+        return isLast ? DECK_COLOR : DECK_SHADOW;
     };
+
     return (
         <div className={classes.root}>
             <div className={classes.deckContainer}>
-                {Array.from({ length: deck.length + discard.length }).map(
-                    (_, i) => {
-                        return (
-                            <div key={i}>
-                                {getShadow(i)}
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    style={{
-                                        bottom: `${i * 5}px`,
-                                    }}
-                                >
-                                    <path
-                                        fill={
-                                            i < discard.length
-                                                ? COOLDOWN_COLOR
-                                                : ARSENAL_COLOR
-                                        }
-                                        d="M 50 75 100 100 50 125 0 100 Z"
-                                    />
-                                    {i === deck.length + discard.length - 1 && (
-                                        <text
-                                            fill="rgba(255, 255, 255, 0.8)"
-                                            x="50"
-                                            fontSize="26px"
-                                            y="110"
-                                            textAnchor="middle"
-                                        >
-                                            {deck.length}
-                                        </text>
-                                    )}
-                                </svg>
-                            </div>
-                        );
-                    }
-                )}
+                {Array.from({ length: deck.length + discard.length }).map((_, i) => {
+                    return (
+                        <div key={i}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                style={{
+                                    bottom: `${i * 5}px`,
+                                }}
+                            >
+                                <path fill={getCardColor(i)} d="M 50 75 100 100 50 125 0 100 Z" />
+                                {i === deck.length + discard.length - 1 && (
+                                    <text fill="rgba(255, 255, 255, 0.8)" x="50" fontSize="26px" y="110" textAnchor="middle">
+                                        {deck.length}
+                                    </text>
+                                )}
+                            </svg>
+                        </div>
+                    );
+                })}
             </div>
             <div className={classes.onCooldown}>Cooldown: {discard.length}</div>
         </div>
