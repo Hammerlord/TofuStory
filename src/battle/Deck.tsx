fc2d2a4fee2272609083922708a6ef04dedeafde
Hammerlord <nepharus@gmail.com>
2021-09-21 21:47:37 -0700

    Adjust deck svg dimensions and show what cards are in the deck/discard on hover

--- src/battle/Deck.tsx
+++ src/battle/Deck.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
+import { compose } from "ramda";
+import { useMemo } from "react";
 import { createUseStyles } from "react-jss";
+import Tooltip from "../view/Tooltip";
 
 const DECK_COLOR = "#176fbd";
 const DECK_SHADOW = "#125896";
@@ -28,6 +30,9 @@ const useStyles = createUseStyles({
         zIndex: -1,
         top: "16px",
     },
+    svg: {
+        overflow: "visible",
+    },
 });
 
 const Deck = ({ deck, discard }) => {
@@ -41,30 +46,50 @@ const Deck = ({ deck, discard }) => {
         return isLast ? DECK_COLOR : DECK_SHADOW;
     };
 
+    const getCountMap = (items: { name: string }[]) => {
+        return items
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .reduce((acc, item) => {
+                acc[item.name] = (acc[item.name] || 0) + 1;
+                return acc;
+            }, {});
+    };
+
+    const getTooltip = (count) => {
+        return Object.entries(count).map(([abilityName, count], i) => (
+            <div key={i}>
+                {abilityName} x{count}
+            </div>
+        ));
+    };
+
+    const deckCount = useMemo(() => compose(getTooltip, getCountMap)(deck), [deck]);
+    const discardCount = useMemo(() => compose(getTooltip, getCountMap)(discard), [discard]);
+
     return (
         <div className={classes.root}>
-            <div className={classes.deckContainer}>
-                {Array.from({ length: deck.length + discard.length }).map((_, i) => {
-                    return (
-                        <div key={i}>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                style={{
-                                    bottom: `${i * 5}px`,
-                                }}
-                            >
-                                <path fill={getCardColor(i)} d="M 50 75 100 100 50 125 0 100 Z" />
-                                {i === deck.length + discard.length - 1 && (
-                                    <text fill="rgba(255, 255, 255, 0.8)" x="50" fontSize="26px" y="110" textAnchor="middle">
-                                        {deck.length}
-                                    </text>
-                                )}
-                            </svg>
-                        </div>
-                    );
-                })}
-            </div>
-            <div className={classes.onCooldown}>Cooldown: {discard.length}</div>
+            <Tooltip title={deckCount} placement={"right"}>
+                <div className={classes.deckContainer}>
+                    <svg viewBox="0 0 100 100" className={classes.svg}>
+                        {Array.from({ length: deck.length + discard.length }).map((_, i) => {
+                            return (
+                                <svg key={i} y={i * -5 + 75} viewBox="0 0 100 100">
+                                    <path fill={getCardColor(i)} d="M 50 0 100 25 50 50 0 25 Z" />
+                                    {i === deck.length + discard.length - 1 && (
+                                        <text fill="rgba(255, 255, 255, 0.8)" x="50" fontSize="26px" y="35" textAnchor="middle">
+                                            {deck.length}
+                                        </text>
+                                    )}
+                                </svg>
+                            );
+                        })}
+                    </svg>
+                </div>
+            </Tooltip>
+
+            <Tooltip title={discardCount} placement={"right"}>
+                <div className={classes.onCooldown}>Cooldown: {discard.length}</div>
+            </Tooltip>
         </div>
     );
 };
