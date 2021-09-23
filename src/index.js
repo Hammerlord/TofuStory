9768dac90603346c79c6fac588b42acb8cbae1b0
Hammerlord <nepharus@gmail.com>
2021-09-22 23:02:49 -0700

    Preload subi to fix an issue with it sometimes being invisible

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { subi } from './images';
+
+/**
+ * HACK: Preload some images or they can be temporarily invisible :/
+ */
+[subi].forEach((image) => {
+    const newImage = new Image();
+    newImage.src = image;
+    window[image] = newImage;
+});
 
 ReactDOM.render(
     <App />,
