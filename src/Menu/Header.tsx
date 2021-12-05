34c2d4e8cbfcc7928b5d06acb5d10e641eed1fde
Hammerlord <nepharus@gmail.com>
2021-12-05 13:17:45 -0800

    Make the card matching game accessible via auxiliary menu

--- src/Menu/Header.tsx
+++ src/Menu/Header.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@material-ui/core";
-import { useState } from "react";
+import { Button, ClickAwayListener, MenuItem, MenuList, Popper } from "@material-ui/core";
+import { useRef, useState } from "react";
 import { createUseStyles } from "react-jss";
 import { Ability } from "../ability/types";
 import { getMaxHP } from "../battle/utils";
 import { Combatant } from "../character/types";
 import { mesoCoinImage } from "../images";
 import { Item } from "../item/types";
+import CardGame from "../scene/CardGame";
 import DeckViewer from "./DeckViewer";
 import Inventory from "./Inventory";
 
@@ -38,28 +39,87 @@ const useStyles = createUseStyles({
         verticalAlign: "middle",
         marginRight: "8px",
     },
+    cardGameContainer: {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translateX(-50%) translateY(-50%)",
+        textAlign: "center",
+        width: "100%",
+        height: "100%",
+        zIndex: "999",
+        background: "rgba(30, 30, 30, 0.9)",
+        color: "rgba(255, 255, 250, 0.9)",
+    },
+    menu: {
+        background: "rgba(50, 46, 46, 0.9)",
+        border: "1px solid rgba(255, 255, 255, 0.5)",
+        borderRadius: "4px",
+        fontFamily: "Barlow",
+        zIndex: "1000",
+        color: "white",
+        maxWidth: 300,
+        padding: "16px",
+    },
 });
 
 const Header = ({ player, deck, onUseItem }: { player: Combatant; deck: Ability[]; onUseItem?: Function }) => {
     const classes = useStyles();
     const [isAbilitiesOpen, setIsAbilitiesOpen] = useState(false);
+    const [extraActivitiesOpen, setExtraActivitiesOpen] = useState(false);
+    const [cardGameDifficulty, setCardGameDifficulty] = useState(null);
+    const [activityMenuAnchor, setActivityMenuAnchor] = useState(null);
+
+    const handleClickActivities = (e) => {
+        setActivityMenuAnchor(e.currentTarget);
+        setExtraActivitiesOpen((prev) => !prev);
+    };
+
+    const handleCardGameDifficultyClick = (difficulty: "easy" | "medium" | "hard") => {
+        setCardGameDifficulty(difficulty);
+        setExtraActivitiesOpen(false);
+    };
 
     return (
-        <div className={classes.headerBar}>
-            <img src={player.image} className={classes.playerPortrait} />{" "}
-            <div className={classes.stats}>
-                {player.HP} / {getMaxHP(player)} HP{" "}
-                <Button variant="contained" color="primary" onClick={() => setIsAbilitiesOpen((prev) => !prev)}>
-                    {deck.length} abilities
-                </Button>
-                <div className={classes.mesos}>
-                    <img src={mesoCoinImage} className={classes.mesoImage} />
-                    {player.mesos}
+        <>
+            {cardGameDifficulty && (
+                <div className={classes.cardGameContainer}>
+                    <CardGame difficulty={cardGameDifficulty} onExit={() => setCardGameDifficulty(null)} />
                 </div>
+            )}
+            <div className={classes.headerBar}>
+                <img src={player.image} className={classes.playerPortrait} />{" "}
+                <div className={classes.stats}>
+                    {player.HP} / {getMaxHP(player)} HP{" "}
+                    <Button variant="contained" color="primary" onClick={() => setIsAbilitiesOpen((prev) => !prev)}>
+                        {deck.length} abilities
+                    </Button>
+                    <div className={classes.mesos}>
+                        <img src={mesoCoinImage} className={classes.mesoImage} />
+                        {player.mesos}
+                    </div>
+                </div>
+                {isAbilitiesOpen && <DeckViewer deck={deck} onClose={() => setIsAbilitiesOpen(false)} />}
+                <Inventory inventory={player.items} onUseItem={onUseItem} />
+                <Button variant="contained" color="primary" onClick={handleClickActivities}>
+                    Other Stuff
+                </Button>
+                {activityMenuAnchor && extraActivitiesOpen && (
+                    <Popper anchorEl={activityMenuAnchor} open={true} placement={"bottom-start"} className={classes.menu}>
+                        <ClickAwayListener onClickAway={() => setExtraActivitiesOpen((prev) => !prev)}>
+                            <div>
+                                Card Matching Minigame
+                                <MenuList>
+                                    <MenuItem onClick={() => handleCardGameDifficultyClick("easy")}>Easy</MenuItem>
+                                    <MenuItem onClick={() => handleCardGameDifficultyClick("medium")}>Medium</MenuItem>
+                                    <MenuItem onClick={() => handleCardGameDifficultyClick("hard")}>Hard</MenuItem>
+                                </MenuList>
+                            </div>
+                        </ClickAwayListener>
+                    </Popper>
+                )}
             </div>
-            {isAbilitiesOpen && <DeckViewer deck={deck} onClose={() => setIsAbilitiesOpen(false)} />}
-            <Inventory inventory={player.items} onUseItem={onUseItem} />
-        </div>
+        </>
     );
 };
 
