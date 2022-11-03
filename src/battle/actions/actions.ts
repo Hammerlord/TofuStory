6888aa56d17ae8fbba0f0c47a355140b462e270e
Hammerlord <nepharus@gmail.com>
2022-11-02 22:54:26 -0700

    Move getMorphMap, getMorphMerge helpers to a separate file

--- src/battle/actions/actions.ts
+++ src/battle/actions/actions.ts
@@ -38,6 +38,7 @@ import { createCombatant } from "./../../enemy/createEnemy";
 import { BATTLE_STATES } from "./../reducer";
 import { TriggerSource } from "./../types";
 import { getUpdatedStats, UpdatedCombatantStats } from "./getUpdatedStats";
+import { getMorphMap, getMorphMerge } from "./morphUtils";
 
 const { drawCards, updateBattle, updateBattleState, pushEventQueue, promptPlayerSelectCards } = battleStateSlice.actions;
 const { updatePlayer } = playerStateSlice.actions;
@@ -662,102 +663,6 @@ const checkHandleSummon = ({ action, actorId, parentSource }: { action: Action; 
             );
         }
     };
-};
-
-/**
- * Handle MORPH_TYPES.MERGE (take n minion(s) and transform them all to z minion(s))
- * This ignores morph conditions
- */
-const getMorphMerge = ({ targets, battlefield, morph }) => {
-    const { minions, modifiers = {} } = morph;
-    const targetIds = targets.map((t: Combatant) => t.id);
-    const { friendly, friendlySide, combatantIndex } = orientate({ combatantId: targetIds[0], ...battlefield });
-    const combatants = friendly.map((combatant: Combatant | null) => {
-        if (targetIds.includes(combatant?.id)) {
-            return null;
-        }
-        return combatant;
-    });
-
-    const summons = [];
-    const getSummonPos = (positionIndex?: number): number => {
-        if (typeof positionIndex === "number") {
-            return positionIndex;
-        }
-
-        // If there is only one mutate target, replace the target
-        if (targets.length === 1) {
-            return combatantIndex;
-        }
-
-        return getRandomItem(getPossibleSummonIndices(friendly));
-    };
-
-    const modifierValues = Object.entries(modifiers).reduce((acc, [property, modifierType]) => {
-        let value = targets.reduce((acc, combatant) => {
-            return acc + (combatant[property] || 0);
-        }, 0); // Default is sum
-        if (modifierType === MORPH_MINION_MODIFIERS.DIVIDE_EVENLY) {
-            value = Math.ceil(value / minions.length);
-        }
-        acc[property] = value;
-        return acc;
-    }, {});
-
-    for (const { minion, positionIndex } of minions) {
-        const pos = getSummonPos(positionIndex);
-        const minionToSummon = typeof minion === "string" ? enemyNameMap[minion] : minion;
-        if (!minionToSummon) {
-            console.warn(`Didn't find a corresponding object for ${minion}. Is the lookup map up to date?`);
-            return;
-        }
-
-        if (typeof pos === "number") {
-            combatants[pos] = {
-                ...createCombatant(minionToSummon),
-                ...modifierValues,
-            };
-
-            summons.push(combatants[pos]);
-        }
-    }
-
-    return { side: friendlySide, combatants, summons };
-};
-
-/**
- * Handle MORPH_TYPES.MAP (for each minion, transform it to another minion)
- */
-const getMorphMap = ({ targets, battlefield, morph }) => {
-    const { minions } = morph;
-    const targetIds = targets.map((t: Combatant) => t.id);
-    const { friendly, friendlySide } = orientate({ combatantId: targetIds[0], ...battlefield });
-    const summons = [];
-    const combatants = friendly.map((combatant) => {
-        if (!targetIds.includes(combatant?.id)) {
-            return combatant;
-        }
-
-        const minion = minions.find((minionConfig) => {
-            const getCalculationTarget = () => combatant; // Current combatant will always be the target
-            return passesConditions({ getCalculationTarget, proc: minionConfig });
-        })?.minion;
-
-        if (minion) {
-            const minionToSummon = typeof minion === "string" ? enemyNameMap[minion] : minion;
-            if (minionToSummon) {
-                const summon = createCombatant(minionToSummon);
-                summons.push(summon);
-                return summon;
-            } else {
-                console.warn(`Didn't find a corresponding object for ${minion}. Is the lookup map up to date?`);
-            }
-        }
-
-        return combatant;
-    });
-
-    return { side: friendlySide, combatants, summons };
 };
 
 const checkHandleMorph = ({
---
+++ src/battle/actions/morphUtils.ts
@@ -0,0 +1,103 @@
+import { MORPH_MINION_MODIFIERS } from "../../ability/types";
+import { Combatant } from "../../character/types";
+import { enemyNameMap } from "../../enemy";
+import { createCombatant } from "../../enemy/createEnemy";
+import { getRandomItem } from "../../utils";
+import { passesConditions } from "../passesConditions";
+import { getPossibleSummonIndices, orientate } from "../utils";
+
+/**
+ * Handle MORPH_TYPES.MERGE (take n minion(s) and transform them all to z minion(s))
+ * This ignores morph conditions
+ */
+export const getMorphMerge = ({ targets, battlefield, morph }) => {
+    const { minions, modifiers = {} } = morph;
+    const targetIds = targets.map((t: Combatant) => t.id);
+    const { friendly, friendlySide, combatantIndex } = orientate({ combatantId: targetIds[0], ...battlefield });
+    const combatants = friendly.map((combatant: Combatant | null) => {
+        if (targetIds.includes(combatant?.id)) {
+            return null;
+        }
+        return combatant;
+    });
+
+    const summons = [];
+    const getSummonPos = (positionIndex?: number): number => {
+        if (typeof positionIndex === "number") {
+            return positionIndex;
+        }
+
+        // If there is only one mutate target, replace the target
+        if (targets.length === 1) {
+            return combatantIndex;
+        }
+
+        return getRandomItem(getPossibleSummonIndices(friendly));
+    };
+
+    const modifierValues = Object.entries(modifiers).reduce((acc, [property, modifierType]) => {
+        let value = targets.reduce((acc, combatant) => {
+            return acc + (combatant[property] || 0);
+        }, 0); // Default is sum
+        if (modifierType === MORPH_MINION_MODIFIERS.DIVIDE_EVENLY) {
+            value = Math.ceil(value / minions.length);
+        }
+        acc[property] = value;
+        return acc;
+    }, {});
+
+    for (const { minion, positionIndex } of minions) {
+        const pos = getSummonPos(positionIndex);
+        const minionToSummon = typeof minion === "string" ? enemyNameMap[minion] : minion;
+        if (!minionToSummon) {
+            console.warn(`Didn't find a corresponding object for ${minion}. Is the lookup map up to date?`);
+            return;
+        }
+
+        if (typeof pos === "number") {
+            combatants[pos] = {
+                ...createCombatant(minionToSummon),
+                ...modifierValues,
+            };
+
+            summons.push(combatants[pos]);
+        }
+    }
+
+    return { side: friendlySide, combatants, summons };
+};
+
+/**
+ * Handle MORPH_TYPES.MAP (for each minion, transform it to another minion)
+ */
+export const getMorphMap = ({ targets, battlefield, morph }) => {
+    const { minions } = morph;
+    const targetIds = targets.map((t: Combatant) => t.id);
+    const { friendly, friendlySide } = orientate({ combatantId: targetIds[0], ...battlefield });
+    const summons = [];
+    const combatants = friendly.map((combatant) => {
+        if (!targetIds.includes(combatant?.id)) {
+            return combatant;
+        }
+
+        const minion = minions.find((minionConfig) => {
+            const getCalculationTarget = () => combatant; // Current combatant will always be the target
+            return passesConditions({ getCalculationTarget, proc: minionConfig });
+        })?.minion;
+
+        if (minion) {
+            const minionToSummon = typeof minion === "string" ? enemyNameMap[minion] : minion;
+            if (minionToSummon) {
+                const summon = createCombatant(minionToSummon);
+                summons.push(summon);
+                return summon;
+            } else {
+                console.warn(`Didn't find a corresponding object for ${minion}. Is the lookup map up to date?`);
+            }
+        }
+
+        return combatant;
+    });
+
+    return { side: friendlySide, combatants, summons };
+};
