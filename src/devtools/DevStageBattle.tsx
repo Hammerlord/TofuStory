f01d2c9b3f73f66c36e210be1ec21578e40e64d4
Hammerlord <nepharus@gmail.com>
2022-11-13 14:38:39 -0800

    Battle sandbox fixtures update

--- src/devtools/DevStageBattle.tsx
+++ src/devtools/DevStageBattle.tsx
@@ -1,17 +1,36 @@
 import { useEffect, useMemo } from "react";
-import { dustDevils, rush, slam, slashBlast2, spikedArmor, warLeap, yell } from "../ability/warrior/warriorAbilities";
+import { Ability, ACTION_TYPES, ANIMATION_TYPES, TARGET_TYPES } from "../ability/types";
+import { shout } from "../ability/warrior/warriorAbilities";
 import { startBattle } from "../battle/actions/phases";
 import BattlefieldContainer from "../battle/BattleView";
 import defaultCharacterProperties from "../character/defaultCharacterProperties";
 import { playerStateSlice } from "../character/playerReducer";
-import { stumpy } from "../enemy/stumpy";
+import { devDummy } from "../enemy/dummy";
 import { useAppDispatch, useAppSelector } from "../hooks";
+import { StarfallMagicSquareImage } from "../images";
 
 const { updatePlayer, updateDeck } = playerStateSlice.actions;
+const dummies = [devDummy, devDummy, devDummy, devDummy, devDummy];
+
+// Dev testing purposes only
+export const deathLaser: Ability = {
+    name: "Death Laser",
+    image: StarfallMagicSquareImage,
+    resourceCost: 0,
+    actions: [
+        {
+            damage: 1000,
+            type: ACTION_TYPES.RANGE_ATTACK,
+            target: TARGET_TYPES.HOSTILE,
+            icon: StarfallMagicSquareImage,
+            animation: ANIMATION_TYPES.BEAM,
+        },
+    ],
+};
 
 const DevStageBattle = () => {
-    const deck = useMemo(() => [spikedArmor, yell, dustDevils, slam, slashBlast2, warLeap, slashBlast2, rush], []);
-    const enemies = useMemo(() => [null, null, stumpy, null, null], []);
+    const deck = useMemo(() => [shout, deathLaser], []);
+    const enemies = useMemo(() => dummies, []);
     const dispatch = useAppDispatch();
     const battle = useAppSelector((state) => state.battle);
     useEffect(() => {
