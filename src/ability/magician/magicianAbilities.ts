6a8658e4e678db754afc077f766d1ddef585fe43
Hammerlord <nepharus@gmail.com>
2024-01-19 22:12:30 -0800

    Basic upgrades for Magician abilities

--- src/ability/magician/magicianAbilities.ts
+++ src/ability/magician/magicianAbilities.ts
@@ -13,12 +13,39 @@ import {
     ACTION_TYPES,
     ANIMATION_TYPES,
     Ability,
+    Action,
     CONDITION_TARGETS,
     EFFECT_CLASSES,
     EFFECT_TYPES,
     TARGET_TYPES,
     TRIGGER_TARGET_TYPES,
 } from "../types";
+
+const energyBolt2: Ability = {
+    name: "Energy Bolt",
+    image: EnergyBoltImage,
+    resourceCost: 0,
+    level: 2,
+    actions: [
+        {
+            damage: 4,
+            target: TARGET_TYPES.HOSTILE,
+            type: ACTION_TYPES.RANGE_ATTACK,
+            animation: ANIMATION_TYPES.ONE_WAY,
+            icon: EnergyBoltProjectileImage,
+            playbackTime: 400,
+            animationOptions: {
+                rotate: -45,
+            },
+        },
+        {
+            type: ACTION_TYPES.EFFECT,
+            target: TARGET_TYPES.SELF,
+            playbackTime: 250,
+            resources: 1,
+        },
+    ],
+};
 
 export const energyBolt: Ability = {
     name: "Energy Bolt",
@@ -43,7 +70,61 @@ export const energyBolt: Ability = {
             resources: 1,
         },
     ],
-    upgrades: [],
+    upgrades: [energyBolt2],
+};
+
+const magicClawAction: Action = {
+    damage: 3,
+    target: TARGET_TYPES.HOSTILE,
+    type: ACTION_TYPES.RANGE_ATTACK,
+    animation: ANIMATION_TYPES.ONE_WAY,
+    icon: MagicClawProjectileImage,
+    playbackTime: 400,
+    bonus: {
+        conditions: [
+            {
+                calculationTarget: CONDITION_TARGETS.ACTOR,
+                hasEffect: "Charged",
+            },
+        ],
+        damage: 2,
+    },
+};
+
+const magicClaw2: Ability = {
+    name: "Magic Claw",
+    resourceCost: 2,
+    image: MagicClawImage,
+    description: "Hits twice",
+    level: 2,
+    actions: [
+        {
+            ...magicClawAction,
+            damage: 4,
+            bonus: {
+                conditions: [
+                    {
+                        calculationTarget: CONDITION_TARGETS.ACTOR,
+                        hasEffect: "Charged",
+                    },
+                ],
+                damage: 3,
+            },
+        },
+        {
+            ...magicClawAction,
+            damage: 4,
+            bonus: {
+                conditions: [
+                    {
+                        calculationTarget: CONDITION_TARGETS.ACTOR,
+                        hasEffect: "Charged",
+                    },
+                ],
+                damage: 3,
+            },
+        },
+    ],
 };
 
 export const magicClaw: Ability = {
@@ -51,46 +132,35 @@ export const magicClaw: Ability = {
     resourceCost: 2,
     image: MagicClawImage,
     description: "Hits twice",
+    actions: [magicClawAction, magicClawAction],
+    upgrades: [magicClaw2],
+};
+
+const magicGuard2: Ability = {
+    name: "Magic Guard",
+    resourceCost: 1,
+    image: MagicGuardImage,
+    level: 2,
     actions: [
         {
-            damage: 3,
-            target: TARGET_TYPES.HOSTILE,
-            type: ACTION_TYPES.RANGE_ATTACK,
-            animation: ANIMATION_TYPES.ONE_WAY,
-            icon: MagicClawProjectileImage,
-            playbackTime: 400,
-            bonus: {
-                conditions: [
-                    {
-                        calculationTarget: CONDITION_TARGETS.ACTOR,
-                        hasEffect: "Charged",
+            target: TARGET_TYPES.SELF,
+            type: ACTION_TYPES.EFFECT,
+            effects: [
+                {
+                    name: "Magic Guard",
+                    description: "Gaining +4 armor every turn for 3 turns",
+                    type: EFFECT_TYPES.NONE,
+                    class: EFFECT_CLASSES.BUFF,
+                    icon: MagicGuardImage,
+                    duration: 3,
+                    onTurnEnd: {
+                        targetType: TRIGGER_TARGET_TYPES.EFFECT_OWNER,
+                        armor: 4,
                     },
-                ],
-                damage: 2,
-            },
-        },
-        {
-            damage: 3,
-            target: TARGET_TYPES.HOSTILE,
-            type: ACTION_TYPES.RANGE_ATTACK,
-            animation: ANIMATION_TYPES.ONE_WAY,
-            icon: MagicClawProjectileImage,
-            playbackTime: 400,
-            animationOptions: {
-                mirrorX: true,
-            },
-            bonus: {
-                conditions: [
-                    {
-                        calculationTarget: CONDITION_TARGETS.ACTOR,
-                        hasEffect: "Charged",
-                    },
-                ],
-                damage: 2,
-            },
+                },
+            ],
         },
     ],
-    upgrades: [],
 };
 
 export const magicGuard: Ability = {
@@ -117,6 +187,21 @@ export const magicGuard: Ability = {
             ],
         },
     ],
+    upgrades: [magicGuard2],
+};
+
+const magicArmor2: Ability = {
+    name: "Magic Armor",
+    resourceCost: 2,
+    image: MagicArmorImage,
+    level: 2,
+    actions: [
+        {
+            target: TARGET_TYPES.SELF,
+            type: ACTION_TYPES.EFFECT,
+            armor: 9,
+        },
+    ],
 };
 
 export const magicArmor: Ability = {
@@ -128,6 +213,23 @@ export const magicArmor: Ability = {
             target: TARGET_TYPES.SELF,
             type: ACTION_TYPES.EFFECT,
             armor: 7,
+        },
+    ],
+    upgrades: [magicArmor2],
+};
+
+const teleport2: Ability = {
+    name: "Teleport",
+    resourceCost: 0,
+    image: TeleportImage,
+    level: 2,
+    actions: [
+        {
+            target: TARGET_TYPES.SELF,
+            type: ACTION_TYPES.EFFECT,
+            drawCards: {
+                amount: 3,
+            },
         },
     ],
 };
@@ -145,6 +247,51 @@ export const teleport: Ability = {
             },
         },
     ],
+    upgrades: [teleport2],
+};
+
+const triboltAction = {
+    damage: 2,
+    target: TARGET_TYPES.RANDOM_HOSTILE,
+    type: ACTION_TYPES.RANGE_ATTACK,
+    animation: ANIMATION_TYPES.ONE_WAY,
+    icon: EnergyBoltProjectileImage,
+    playbackTime: 400,
+    animationOptions: {
+        rotate: -45,
+    },
+    targetArea: 1,
+    bonus: {
+        conditions: [
+            {
+                calculationTarget: CONDITION_TARGETS.ACTOR,
+                hasEffect: "Charged",
+            },
+        ],
+        damage: 2,
+    },
+};
+
+const tribolt2: Ability = {
+    name: "Tribolt",
+    image: TriboltImage,
+    resourceCost: 1,
+    description: "Randomly hits the target or adjacent enemies, x3",
+    level: 2,
+    actions: [
+        {
+            ...triboltAction,
+            damage: 3,
+        },
+        {
+            ...triboltAction,
+            damage: 3,
+        },
+        {
+            ...triboltAction,
+            damage: 3,
+        },
+    ],
 };
 
 export const tribolt: Ability = {
@@ -152,70 +299,6 @@ export const tribolt: Ability = {
     image: TriboltImage,
     resourceCost: 1,
     description: "Randomly hits the target or adjacent enemies, x3",
-    actions: [
-        {
-            damage: 2,
-            target: TARGET_TYPES.RANDOM_HOSTILE,
-            type: ACTION_TYPES.RANGE_ATTACK,
-            animation: ANIMATION_TYPES.ONE_WAY,
-            icon: EnergyBoltProjectileImage,
-            playbackTime: 400,
-            animationOptions: {
-                rotate: -45,
-            },
-            targetArea: 1,
-            bonus: {
-                conditions: [
-                    {
-                        calculationTarget: CONDITION_TARGETS.ACTOR,
-                        hasEffect: "Charged",
-                    },
-                ],
-                damage: 2,
-            },
-        },
-        {
-            damage: 2,
-            target: TARGET_TYPES.RANDOM_HOSTILE,
-            type: ACTION_TYPES.RANGE_ATTACK,
-            animation: ANIMATION_TYPES.ONE_WAY,
-            icon: EnergyBoltProjectileImage,
-            playbackTime: 400,
-            targetArea: 1,
-            animationOptions: {
-                rotate: -45,
-            },
-            bonus: {
-                conditions: [
-                    {
-                        calculationTarget: CONDITION_TARGETS.ACTOR,
-                        hasEffect: "Charged",
-                    },
-                ],
-                damage: 2,
-            },
-        },
-        {
-            damage: 2,
-            target: TARGET_TYPES.RANDOM_HOSTILE,
-            type: ACTION_TYPES.RANGE_ATTACK,
-            animation: ANIMATION_TYPES.ONE_WAY,
-            icon: EnergyBoltProjectileImage,
-            playbackTime: 400,
-            targetArea: 1,
-            animationOptions: {
-                rotate: -45,
-            },
-            bonus: {
-                conditions: [
-                    {
-                        calculationTarget: CONDITION_TARGETS.ACTOR,
-                        hasEffect: "Charged",
-                    },
-                ],
-                damage: 2,
-            },
-        },
-    ],
-    upgrades: [],
+    actions: [triboltAction, triboltAction, triboltAction],
+    upgrades: [tribolt2],
 };
