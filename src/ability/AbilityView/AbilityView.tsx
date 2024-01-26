f12ce64eb985806c5c6b01d2e4e44788e80acf21
Hammerlord <nepharus@gmail.com>
2024-01-25 21:06:46 -0800

    Update Hand animation

--- src/ability/AbilityView/AbilityView.tsx
+++ src/ability/AbilityView/AbilityView.tsx
@@ -151,166 +151,172 @@ interface AbilityViewProps {
     deck?: Ability[];
     hand?: Ability[];
     discard?: Ability[];
+    className?: string;
 }
 
-const AbilityView = forwardRef(({ onClick, isSelected, ability, player, deck = [], hand = [], discard = [] }: AbilityViewProps, ref) => {
-    const classes = useStyles();
-    const { actions = [], name, minion, image, description, overrideBodyText, removeAfterTurn, depletedOnUse, preemptive } = ability;
-    const { target: targetType, type, damage, secondaryDamage, destroyArmor = 0, ricochet, targetArea, numTargets } = actions[0] || {};
-    const cardImage = minion?.image || image;
-    let imageNode = null;
-
-    if (typeof cardImage === "string") {
-        imageNode = <img src={cardImage} className={classes.portrait} draggable="false" />;
-    } else if (typeof image === "function") {
-        const ImageNode = image as Function;
-        imageNode = (
-            <div>
-                <ImageNode className={classes.portrait} />
-            </div>
-        );
-    }
-
-    const { baseDamage, damageBonusFromConditions } = getDamageStatistics({ ability, player, deck, hand, discard });
-    const { bonusFromConditions: armorBonusFromConditions } = getArmorStatistics({ ability, player });
-    const interpolatedDescription = Handlebars.compile(description || "")({ damage: baseDamage });
-
-    let hasMultiplier = false;
-    let armorCornerIcon = false;
-    let healingCornerIcon = false;
-
-    const {
-        healing,
-        armor,
-        damage: selfDamage,
-        resourceGain,
-    } = ability.actions
-        .filter(({ target }) => target === TARGET_TYPES.SELF || target === TARGET_TYPES.FRIENDLY)
-        .reduce((acc: any, action: Action) => {
-            const { healing = 0, damage = 0, armor = 0, resources = 0 } = action;
-            const multiplier = getMultiplier({ multiplier: action.multiplier, actor: { combatant: player }, deck, hand, discard });
-            if (multiplier > 1) {
-                hasMultiplier = true;
-            }
-            return {
-                healing: (acc.healing || 0) + healing * multiplier,
-                armor: (acc.armor || 0) + armor * multiplier,
-                damage: (acc.damage || 0) + damage * multiplier,
-                resourceGain: (acc.resourceGain || 0) + resources * multiplier,
-            };
-        }, {}) as any;
-
-    const cornerIcon = (() => {
-        if (baseDamage > 0) {
-            return <DamageIcon ability={ability} player={player} deck={deck} hand={hand} discard={discard} />;
+const AbilityView = forwardRef(
+    ({ onClick, isSelected, ability, player, deck = [], hand = [], discard = [], className }: AbilityViewProps, ref) => {
+        const classes = useStyles();
+        if (className) {
+            console.log(classNames(classes.root, className));
         }
+        const { actions = [], name, minion, image, description, overrideBodyText, removeAfterTurn, depletedOnUse, preemptive } = ability;
+        const { target: targetType, type, damage, secondaryDamage, destroyArmor = 0, ricochet, targetArea, numTargets } = actions[0] || {};
+        const cardImage = minion?.image || image;
+        let imageNode = null;
 
-        if (armor > 0) {
-            armorCornerIcon = true;
-            return <ArmorIcon ability={ability} player={player} />;
-        }
-
-        if (healing > 0) {
-            healingCornerIcon = true;
-            return (
-                <Icon
-                    icon={<HeartIcon />}
-                    text={healing}
-                    className={classNames({
-                        [classes.highlightText]: hasMultiplier,
-                    })}
-                />
+        if (typeof cardImage === "string") {
+            imageNode = <img src={cardImage} className={classes.portrait} draggable="false" />;
+        } else if (typeof image === "function") {
+            const ImageNode = image as Function;
+            imageNode = (
+                <div>
+                    <ImageNode className={classes.portrait} />
+                </div>
             );
         }
 
-        return <div className={classes.iconPlaceholder} />;
-    })();
+        const { baseDamage, damageBonusFromConditions } = getDamageStatistics({ ability, player, deck, hand, discard });
+        const { bonusFromConditions: armorBonusFromConditions } = getArmorStatistics({ ability, player });
+        const interpolatedDescription = Handlebars.compile(description || "")({ damage: baseDamage });
 
-    const hasBonus = damageBonusFromConditions || armorBonusFromConditions;
+        let hasMultiplier = false;
+        let armorCornerIcon = false;
+        let healingCornerIcon = false;
 
-    return (
-        <AbilityTooltip ability={ability} deck={deck} hand={hand} discard={discard}>
-            <div className={classes.root}>
-                <div
-                    onClick={onClick}
-                    className={classNames(classes.inner, {
-                        [classes.selectedAbility]: isSelected,
-                        [classes.ephemeral]: removeAfterTurn,
-                        [classes.glow]: hasBonus,
-                    })}
-                    style={{ borderTop: `3px solid ${getAbilityColor(ability)}` }}
-                >
-                    <span className={classes.header}>
-                        {cornerIcon}
-                        <span className={classes.name}>
-                            {name}{" "}
-                            {ability.level > 1 &&
-                                Array.from({ length: ability.level })
-                                    .map(() => "⋆")
-                                    .join("")}
-                        </span>{" "}
-                        <AbilityResourceIcon ability={ability} playerClass={player?.class} />
-                    </span>
-                    <div className={classes.portraitContainer}>{imageNode}</div>
-                    <div className={classes.body}>
-                        {preemptive && <div className={classes.bold}>Pre-emptive</div>}
-                        {removeAfterTurn && <div className={classes.bold}>Ephemeral</div>}
-                        {depletedOnUse && <div className={classes.bold}>Deplete</div>}
-                        {ability.reusable && <div className={classes.bold}>Reusable</div>}
-                        <DrawCards ability={ability} playerClass={player?.class} />
-                        {!overrideBodyText && <Debuffs effects={getAllEffects(ability)} />}
-                        {ricochet && (
-                            <div>
-                                Bounces to up to {numTargets} other targets{" "}
-                                {secondaryDamage && (
-                                    <>
-                                        for <Icon icon={<CrossedSwordsIcon />} text={secondaryDamage} size={"sm"} />{" "}
-                                    </>
-                                )}
-                            </div>
-                        )}
-                        {!healingCornerIcon && healing > 0 && (
-                            <div>
-                                Heal for <Icon icon={<HeartIcon />} text={healing} size={"sm"} />
-                            </div>
-                        )}
-                        {!armorCornerIcon && armor > 0 && (
-                            <div>
-                                Gain <Icon icon={<ShieldIcon />} text={armor} size={"sm"} />
-                            </div>
-                        )}
-                        {resourceGain > 0 && (
-                            <div>
-                                Gain <ResourceIcon text={resourceGain} size={"sm"} playerClass={player?.class} />
-                            </div>
-                        )}
-                        {selfDamage > 0 && (
-                            <div>
-                                Self-inflict <Icon icon={<CrossedSwordsIcon />} text={selfDamage} size={"sm"} />
-                            </div>
-                        )}
-                        {!overrideBodyText && <Buffs ability={ability} player={player} />}
-                        <CardsToAdd ability={ability} />
-                        {!overrideBodyText && <BonusView ability={ability} player={player} deck={deck} hand={hand} discard={discard} />}
-                        <RadiateView ability={ability} player={player} deck={deck} hand={hand} discard={discard} />
-                        {destroyArmor > 0 && <div>Destroy {destroyArmor * 100}% armor</div>}
-                        {interpolatedDescription && <div>{interpolatedDescription}</div>}
+        const {
+            healing,
+            armor,
+            damage: selfDamage,
+            resourceGain,
+        } = ability.actions
+            .filter(({ target }) => target === TARGET_TYPES.SELF || target === TARGET_TYPES.FRIENDLY)
+            .reduce((acc: any, action: Action) => {
+                const { healing = 0, damage = 0, armor = 0, resources = 0 } = action;
+                const multiplier = getMultiplier({ multiplier: action.multiplier, actor: { combatant: player }, deck, hand, discard });
+                if (multiplier > 1) {
+                    hasMultiplier = true;
+                }
+                return {
+                    healing: (acc.healing || 0) + healing * multiplier,
+                    armor: (acc.armor || 0) + armor * multiplier,
+                    damage: (acc.damage || 0) + damage * multiplier,
+                    resourceGain: (acc.resourceGain || 0) + resources * multiplier,
+                };
+            }, {}) as any;
+
+        const cornerIcon = (() => {
+            if (baseDamage > 0) {
+                return <DamageIcon ability={ability} player={player} deck={deck} hand={hand} discard={discard} />;
+            }
+
+            if (armor > 0) {
+                armorCornerIcon = true;
+                return <ArmorIcon ability={ability} player={player} />;
+            }
+
+            if (healing > 0) {
+                healingCornerIcon = true;
+                return (
+                    <Icon
+                        icon={<HeartIcon />}
+                        text={healing}
+                        className={classNames({
+                            [classes.highlightText]: hasMultiplier,
+                        })}
+                    />
+                );
+            }
+
+            return <div className={classes.iconPlaceholder} />;
+        })();
+
+        const hasBonus = damageBonusFromConditions || armorBonusFromConditions;
+
+        return (
+            <AbilityTooltip ability={ability} deck={deck} hand={hand} discard={discard}>
+                <div className={classNames(classes.root, className)}>
+                    <div
+                        onClick={onClick}
+                        className={classNames(classes.inner, {
+                            [classes.selectedAbility]: isSelected,
+                            [classes.ephemeral]: removeAfterTurn,
+                            [classes.glow]: hasBonus,
+                        })}
+                        style={{ borderTop: `3px solid ${getAbilityColor(ability)}` }}
+                    >
+                        <span className={classes.header}>
+                            {cornerIcon}
+                            <span className={classes.name}>
+                                {name}{" "}
+                                {ability.level > 1 &&
+                                    Array.from({ length: ability.level })
+                                        .map(() => "⋆")
+                                        .join("")}
+                            </span>{" "}
+                            <AbilityResourceIcon ability={ability} playerClass={player?.class} />
+                        </span>
+                        <div className={classes.portraitContainer}>{imageNode}</div>
+                        <div className={classes.body}>
+                            {preemptive && <div className={classes.bold}>Pre-emptive</div>}
+                            {removeAfterTurn && <div className={classes.bold}>Ephemeral</div>}
+                            {depletedOnUse && <div className={classes.bold}>Deplete</div>}
+                            {ability.reusable && <div className={classes.bold}>Reusable</div>}
+                            <DrawCards ability={ability} playerClass={player?.class} />
+                            {!overrideBodyText && <Debuffs effects={getAllEffects(ability)} />}
+                            {ricochet && (
+                                <div>
+                                    Bounces to up to {numTargets} other targets{" "}
+                                    {secondaryDamage && (
+                                        <>
+                                            for <Icon icon={<CrossedSwordsIcon />} text={secondaryDamage} size={"sm"} />{" "}
+                                        </>
+                                    )}
+                                </div>
+                            )}
+                            {!healingCornerIcon && healing > 0 && (
+                                <div>
+                                    Heal for <Icon icon={<HeartIcon />} text={healing} size={"sm"} />
+                                </div>
+                            )}
+                            {!armorCornerIcon && armor > 0 && (
+                                <div>
+                                    Gain <Icon icon={<ShieldIcon />} text={armor} size={"sm"} />
+                                </div>
+                            )}
+                            {resourceGain > 0 && (
+                                <div>
+                                    Gain <ResourceIcon text={resourceGain} size={"sm"} playerClass={player?.class} />
+                                </div>
+                            )}
+                            {selfDamage > 0 && (
+                                <div>
+                                    Self-inflict <Icon icon={<CrossedSwordsIcon />} text={selfDamage} size={"sm"} />
+                                </div>
+                            )}
+                            {!overrideBodyText && <Buffs ability={ability} player={player} />}
+                            <CardsToAdd ability={ability} />
+                            {!overrideBodyText && <BonusView ability={ability} player={player} deck={deck} hand={hand} discard={discard} />}
+                            <RadiateView ability={ability} player={player} deck={deck} hand={hand} discard={discard} />
+                            {destroyArmor > 0 && <div>Destroy {destroyArmor * 100}% armor</div>}
+                            {interpolatedDescription && <div>{interpolatedDescription}</div>}
+                        </div>
+                        <div className={classes.footer}>
+                            {<Area ability={ability} player={player} deck={deck} hand={hand} discard={discard} />}
+                            <AbilityTypeView targetType={targetType} type={type} minion={minion} />
+                            {minion && (
+                                <div className={classes.minionStats}>
+                                    <Icon icon={<HeartIcon />} text={minion.maxHP} className={classes.minionHP} />
+                                    <Icon icon={<CrossedSwordsIcon />} text={minion.damage} className={classes.minionDamage} />
+                                </div>
+                            )}
+                        </div>
                     </div>
-                    <div className={classes.footer}>
-                        {<Area ability={ability} player={player} deck={deck} hand={hand} discard={discard} />}
-                        <AbilityTypeView targetType={targetType} type={type} minion={minion} />
-                        {minion && (
-                            <div className={classes.minionStats}>
-                                <Icon icon={<HeartIcon />} text={minion.maxHP} className={classes.minionHP} />
-                                <Icon icon={<CrossedSwordsIcon />} text={minion.damage} className={classes.minionDamage} />
-                            </div>
-                        )}
-                    </div>
+                    <div className={classes.refContainer} ref={ref as any} />
                 </div>
-                <div className={classes.refContainer} ref={ref as any} />
-            </div>
-        </AbilityTooltip>
-    );
-});
+            </AbilityTooltip>
+        );
+    }
+);
 
 export default AbilityView;
--- src/battle/Hand.tsx
+++ src/battle/Hand.tsx
@@ -1,38 +1,60 @@
 import { useEffect, useState } from "react";
 import AbilityView from "../ability/AbilityView/AbilityView";
 import { HandAbility } from "../ability/types";
+import { createUseStyles } from "react-jss";
+import classNames from "classnames";
+
+const useStyles = createUseStyles({
+    "@keyframes slideIn": {
+        from: {
+            transform: "translate(-25%, 0)",
+            opacity: 0,
+        },
+        to: {
+            transform: "translate(0, 0)",
+            opacity: 1,
+        },
+    },
+    slideIn: {
+        animation: "$slideIn",
+        transitionTimingFunction: "ease-in",
+        animationDuration: 500,
+        animationIterationCount: 1,
+    },
+    "@keyframes slideOut": {
+        from: {
+            transform: "translate(0, 0)",
+            opacity: 1,
+        },
+        to: {
+            transform: "translate(0, -50%)",
+            opacity: 0,
+        },
+    },
+    slideOut: {
+        animation: "$slideOut",
+        transitionTimingFunction: "ease-out",
+        animationDuration: 500,
+        animationIterationCount: 1,
+    },
+});
 
 const Hand = ({ deck, hand, discard, onAbilityClick, selectedAbilityId, className, player, refs }) => {
     const [oldHand, setOldHand] = useState([]);
     const [isPlayingAnimation, setIsPlayingAnimation] = useState(false);
+    const classes = useStyles();
 
     useEffect(() => {
-        let timeout;
-        if (hand.length > 0 && oldHand.length === 0) {
-            // Just a rudimentary card draw "animation" for now
-
-            let currentHand = [];
-            const addCard = (newHand) => {
-                currentHand = [...currentHand, newHand.shift()];
-                setOldHand(currentHand);
-                if (newHand.length) {
-                    timeout = setTimeout(() => {
-                        addCard(newHand);
-                    }, 200);
-                } else {
-                    setIsPlayingAnimation(false);
-                }
-            };
-
-            setIsPlayingAnimation(true);
-            addCard(hand.slice());
-        } else {
+        setIsPlayingAnimation(true);
+        const timeout = setTimeout(() => {
             setOldHand(hand);
-        }
+            setIsPlayingAnimation(false);
+        }, 490);
 
         return () => {
             clearTimeout(timeout);
             setOldHand(hand);
+            setIsPlayingAnimation(false);
         };
     }, [hand]);
 
@@ -42,9 +64,25 @@ const Hand = ({ deck, hand, discard, onAbilityClick, selectedAbilityId, classNam
         }
     };
 
+    const isCardInHand = (card: HandAbility, hand: HandAbility[]): boolean => {
+        return hand.some((c) => c.instanceId === card.instanceId);
+    };
+
+    const getMergedHand = () => {
+        const uniqueHand = [...oldHand];
+        hand.forEach((card) => {
+            if (!isCardInHand(card, uniqueHand)) {
+                uniqueHand.push(card);
+            }
+        });
+        return uniqueHand;
+    };
+
+    const handToDisplay = hand !== oldHand ? getMergedHand() : hand;
+
     return (
         <div className={className}>
-            {oldHand.map((ability: HandAbility, i: number) => (
+            {handToDisplay.map((ability: HandAbility, i: number) => (
                 <AbilityView
                     onClick={(e) => handleAbilityClick(e, ability.instanceId)}
                     isSelected={selectedAbilityId === ability.instanceId}
@@ -55,6 +93,10 @@ const Hand = ({ deck, hand, discard, onAbilityClick, selectedAbilityId, classNam
                     deck={deck}
                     hand={hand}
                     discard={discard}
+                    className={classNames({
+                        [classes.slideIn]: !isCardInHand(ability, oldHand),
+                        [classes.slideOut]: !isCardInHand(ability, hand),
+                    })}
                 />
             ))}
         </div>
