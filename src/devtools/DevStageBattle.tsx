384b69ca4e47bd0cfed078eb81d7585243377f54
Hammerlord <nepharus@gmail.com>
2024-02-06 22:19:50 -0800

    Move Death Laser to its own file

--- src/devtools/DevStageBattle.tsx
+++ src/devtools/DevStageBattle.tsx
@@ -1,36 +1,16 @@
 import { useEffect, useMemo } from "react";
-import { Ability, ACTION_TYPES, ANIMATION_TYPES, TARGET_TYPES } from "../ability/types";
-import { shout } from "../ability/warrior/warriorAbilities";
-import { startBattle } from "../battle/actions/phases";
 import BattlefieldContainer from "../battle/BattleView";
-import defaultCharacterProperties, { wizardProperties } from "../character/defaultCharacterProperties";
+import { startBattle } from "../battle/actions/phases";
+import { wizardProperties } from "../character/defaultCharacterProperties";
 import { playerStateSlice } from "../character/playerReducer";
 import { devDummy } from "../enemy/dummy";
-import { useAppDispatch, useAppSelector } from "../hooks";
-import { StarfallMagicSquareImage } from "../images";
 import { faust, ghostlyPuppeteerL, ghostlyPuppeteerR } from "../enemy/faust";
-import { JOB_CARD_MAP } from "../ability";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { deathLaser } from "./deathLaser";
 
 const { updatePlayer, updateDeck } = playerStateSlice?.actions || {};
 const dummies = [devDummy, devDummy, devDummy, devDummy, devDummy];
 const other = [ghostlyPuppeteerL, null, faust, null, ghostlyPuppeteerR];
-
-// Dev testing purposes only
-export const deathLaser: Ability = {
-    name: "Death Laser",
-    image: StarfallMagicSquareImage,
-    resourceCost: 0,
-    actions: [
-        {
-            damage: 1000,
-            area: 1,
-            type: ACTION_TYPES.RANGE_ATTACK,
-            target: TARGET_TYPES.HOSTILE,
-            icon: StarfallMagicSquareImage,
-            animation: ANIMATION_TYPES.BEAM,
-        },
-    ],
-};
 
 const DevStageBattle = () => {
     const deck = useMemo(() => [deathLaser], []);
---
+++ src/devtools/deathLaser.ts
@@ -0,0 +1,19 @@
+import { ACTION_TYPES, ANIMATION_TYPES, Ability, TARGET_TYPES } from "../ability/types";
+import { StarfallMagicSquareImage } from "../images";
+
+// Dev testing purposes only
+export const deathLaser: Ability = {
+    name: "Death Laser",
+    image: StarfallMagicSquareImage,
+    resourceCost: 0,
+    actions: [
+        {
+            damage: 1000,
+            area: 1,
+            type: ACTION_TYPES.RANGE_ATTACK,
+            target: TARGET_TYPES.HOSTILE,
+            icon: StarfallMagicSquareImage,
+            animation: ANIMATION_TYPES.BEAM,
+        },
+    ],
+};
