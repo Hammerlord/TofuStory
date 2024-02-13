2788ff9d0043546d2b6148e313cf69365de08ff6
Hammerlord <nepharus@gmail.com>
2024-02-13 15:03:12 -0800

    Do not display hourglass on the card effect description if it lasts forever

--- src/ability/AbilityView/BonusView.tsx
+++ src/ability/AbilityView/BonusView.tsx
@@ -91,9 +91,12 @@ const BonusView = ({ ability, player, deck, hand, discard }) => {
                             Apply{" "}
                             {effects.map((e: Effect, i) => (
                                 <span key={e.name}>
-                                    <Icon icon={e.icon} size={"sm"} /> <Icon icon={<HourglassIcon />} text={e.duration} size={"sm"} />{" "}
+                                    <Icon icon={e.icon} size={"sm"} />{" "}
+                                    {typeof e.duration === "number" && e.duration !== Infinity && (
+                                        <Icon icon={<HourglassIcon />} text={e.duration} size={"sm"} />
+                                    )}
                                 </span>
-                            ))}{" "}
+                            ))}
                         </span>
                     );
                 }
