585417446ee15be55a67fc69693d26aec6a506db
Hammerlord <nepharus@gmail.com>
2024-02-24 00:18:05 -0800

    Fix an issue where swinging animations would get stuck in the starting position if used consecutively (switch to HTMLElement animate)

--- src/character/Weapon.tsx
+++ src/character/Weapon.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import { useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { createUseStyles } from "react-jss";
 import { ACTION_TYPES, Action, Effect } from "../ability/types";
 import { getRotationToFaceTarget, getTargetPoints } from "./animations";
@@ -38,53 +38,6 @@ const useStyles = createUseStyles({
         transitionDuration: "0.1s",
         transitionProperty: "transform",
     },
-    "@keyframes swinging": {
-        "0%": {
-            transform: "rotate(0deg)",
-        },
-        "15%": {
-            transform: "rotate(0deg)",
-        },
-        "35%": {
-            transform: "rotate(-35deg)",
-        },
-        "40%": {
-            transform: "rotate(20deg)",
-        },
-        "75%": {
-            transform: "rotate(120deg)",
-        },
-        "90%": {
-            transform: "rotate(50deg)",
-        },
-        "100%": {
-            transform: "unset",
-        },
-    },
-    swing: {
-        animationDuration: "0.5s",
-        animationName: "$swinging",
-        transition: "1s filter ease-out, 1s -webkit-filter ease-out",
-    },
-    "@keyframes whirling": {
-        "0%": {
-            transform: "rotate(0deg)",
-        },
-        "35%": {
-            transform: "rotate(0deg)",
-        },
-        "90%": {
-            transform: "rotate(520deg)",
-        },
-        "100%": {
-            transform: "rotate(360deg)",
-        },
-    },
-    whirl: {
-        animationDuration: "0.6s",
-        animationName: "$whirling",
-        transition: "1s filter ease-in-out, 1s -webkit-filter ease-in-out",
-    },
     "@keyframes glow": {
         "0%": {
             filter: "brightness(1) drop-shadow(0 0 1px #fffee8) drop-shadow(0 0 1px #fffee8)",
@@ -104,6 +57,60 @@ const useStyles = createUseStyles({
     },
 });
 
+const swing = ({ object, playbackTime = 500 }) => {
+    const animationFrames = [
+        {
+            transform: "rotate(0deg)",
+            offset: 0.25,
+            easing: "ease-out",
+        },
+        {
+            transform: "rotate(-35deg)",
+            offset: 0.5,
+        },
+        {
+            transform: "rotate(20deg)",
+            offset: 0.55,
+        },
+        {
+            transform: "rotate(120deg)",
+            offset: 0.75,
+        },
+        {
+            transform: "rotate(50deg)",
+            easing: "ease-in",
+            offset: 0.9,
+        },
+    ];
+
+    return object.animate(animationFrames, {
+        duration: playbackTime,
+    });
+};
+
+const whirl = ({ object, playbackTime = 500 }) => {
+    const animationFrames = [
+        {
+            transform: "rotate(0deg)",
+            offset: 0.35,
+            easing: "ease-out",
+        },
+        {
+            transform: "rotate(520deg)",
+            offset: 0.9,
+        },
+        {
+            transform: "rotate(360deg)",
+            offset: 1,
+            easing: "ease-in",
+        },
+    ];
+
+    return object.animate(animationFrames, {
+        duration: playbackTime,
+    });
+};
+
 const Weapon = ({
     image,
     options,
@@ -121,6 +128,8 @@ const Weapon = ({
 }) => {
     const classes = useStyles(options as any);
     const { type, area } = action || {};
+    const weaponRef = useRef();
+    const animationRef = useRef();
 
     const rotation = useMemo(() => {
         if (type !== ACTION_TYPES.ATTACK || !target || !wielderRef || !action || area) {
@@ -133,6 +142,19 @@ const Weapon = ({
 
     const isGlowing = useMemo(() => wielder?.effects?.some((e: Effect) => e.weaponAnimation === "glow"), [wielder?.effects]);
 
+    useEffect(() => {
+        //@ts-ignore
+        animationRef.current?.cancel();
+        if (!weaponRef.current || type !== ACTION_TYPES.ATTACK) {
+            return;
+        }
+        if (area === 1) {
+            animationRef.current = swing({ object: weaponRef.current });
+        } else if (area >= 2) {
+            animationRef.current = whirl({ object: weaponRef.current });
+        }
+    }, [action]);
+
     if (!image) {
         return null;
     }
@@ -142,12 +164,11 @@ const Weapon = ({
             className={classNames(classes.root, {
                 [classes.idle]: type !== ACTION_TYPES.ATTACK,
                 [classes.stab]: type === ACTION_TYPES.ATTACK && !area,
-                [classes.swing]: type === ACTION_TYPES.ATTACK && area >= 1,
-                [classes.whirl]: type === ACTION_TYPES.ATTACK && area >= 2,
             })}
             style={{
                 transform: rotation ? `rotate(${rotation}deg)` : "unset",
             }}
+            ref={weaponRef}
         >
             <div
                 className={classNames({
