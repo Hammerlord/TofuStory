a8c3dc789e87501fe58001d879eae0da2aca2fc9
Hammerlord <nepharus@gmail.com>
2024-02-27 01:25:37 -0800

    Simple buff/debuff divider in the effect icons

--- src/character/effects/EffectIcons.tsx
+++ src/character/effects/EffectIcons.tsx
@@ -4,10 +4,44 @@ import { CombatEffect, EFFECT_CLASSES, Effect } from "../../ability/types";
 import EffectGroupIcon from "../../icon/EffectGroupIcon";
 import { Combatant, Player } from "../types";
 import { Event } from "../../battle/types";
+import { COLOR_RARITY_UNCOMMON } from "../../constants";
 
+const indicatorSize = 6;
 const useStyles = createUseStyles({
+    root: {
+        "& hr": {
+            opacity: 0.8,
+            width: "100%",
+            height: 0,
+            borderTop: 0,
+            marginTop: 8,
+            borderBottom: "1px solid rgba(0, 0, 0, 0.2)",
+        },
+    },
     buffs: {
-        marginBottom: "4px",
+        minHeight: 24,
+    },
+    debuffs: {
+        minHeight: 24,
+    },
+    divider: {
+        display: "flex",
+    },
+    up: {
+        borderLeft: `${indicatorSize}px solid transparent`,
+        borderRight: `${indicatorSize}px solid transparent`,
+        borderBottom: `${indicatorSize}px solid ${COLOR_RARITY_UNCOMMON}`,
+        marginBottom: 1,
+    },
+    down: {
+        borderLeft: `${indicatorSize}px solid transparent`,
+        borderRight: `${indicatorSize}px solid transparent`,
+        borderTop: `${indicatorSize}px solid #a74848`,
+    },
+    dividerIndicator: {
+        display: "flex",
+        flexDirection: "column",
+        margin: "2px 8px",
     },
 });
 
@@ -42,7 +76,7 @@ const EffectIconsContainer = ({ combatant, isSilenced, event }: { combatant: Com
     };
 
     return (
-        <>
+        <div className={classes.root}>
             <div className={classes.buffs}>
                 {getEffectGroups(buffs).map((effects: CombatEffect[], i) => (
                     <EffectGroupIcon
@@ -54,7 +88,16 @@ const EffectIconsContainer = ({ combatant, isSilenced, event }: { combatant: Com
                     />
                 ))}
             </div>
-            <div>
+            <div className={classes.divider}>
+                <hr />
+                <div className={classes.dividerIndicator}>
+                    <span className={classes.up} />
+                    <span className={classes.down} />
+                </div>
+                <hr />
+            </div>
+
+            <div className={classes.debuffs}>
                 {getEffectGroups(debuffs).map((effects: CombatEffect[], i) => (
                     <EffectGroupIcon
                         effects={effects}
@@ -65,7 +108,7 @@ const EffectIconsContainer = ({ combatant, isSilenced, event }: { combatant: Com
                     />
                 ))}
             </div>
-        </>
+        </div>
     );
 };
 
