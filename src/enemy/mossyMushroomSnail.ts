9ee989c4c3e68ca001d0ab1ab7550bff1a985ca2
Hammerlord <nepharus@gmail.com>
2024-03-11 15:23:54 -0700

    Retune Mossy Snail/Mushroom

--- src/enemy/mossyMushroomSnail.ts
+++ src/enemy/mossyMushroomSnail.ts
@@ -1,12 +1,12 @@
 import { preventArmorDecay } from "../ability/Effects";
 import { ACTION_TYPES, ANIMATION_TYPES, EFFECT_CLASSES, EFFECT_TYPES, Minion, TARGET_TYPES } from "../ability/types";
-import { GreenFairiesImage, MossyMushroomImage, MossySnailImage, MushroomOmokImage } from "../images";
+import { GreenFairiesImage, MossyMushroomImage, MossySnailImage, MushroomOmokImage, RedSnailShellImage } from "../images";
 import { attack } from "./abilities";
 import { agedShell, weightedShell } from "./effect";
 
 export const mossyMushroom: Minion = {
     name: "Mossy Mushroom",
-    maxHP: 200,
+    maxHP: 150,
     isElite: true,
     image: MossyMushroomImage,
     resources: 2,
@@ -17,7 +17,7 @@ export const mossyMushroom: Minion = {
                 {
                     type: ACTION_TYPES.ATTACK,
                     target: TARGET_TYPES.HOSTILE,
-                    damage: 3,
+                    damage: 4,
                 },
             ],
         },
@@ -62,7 +62,7 @@ export const mossyMushroom: Minion = {
 
 export const mossySnail: Minion = {
     name: "Mossy Snail",
-    maxHP: 100,
+    maxHP: 50,
     armor: 100,
     image: MossySnailImage,
     isElite: true,
@@ -73,12 +73,13 @@ export const mossySnail: Minion = {
                 {
                     type: ACTION_TYPES.ATTACK,
                     target: TARGET_TYPES.HOSTILE,
-                    damage: 2,
+                    damage: 3,
                 },
             ],
         },
         {
             name: "Rollout",
+            image: RedSnailShellImage,
             castTime: 1,
             channelDuration: 3,
             resourceCost: 3,
@@ -87,8 +88,15 @@ export const mossySnail: Minion = {
                     type: ACTION_TYPES.ATTACK,
                     target: TARGET_TYPES.HOSTILE,
                     animation: ANIMATION_TYPES.YOYO,
+                    animationOptions: {
+                        ricochet: true,
+                    },
                     playbackTime: 600,
-                    damage: 3,
+                    description: "Bounces to 2 other targets for 3 damage.",
+                    damage: 6,
+                    secondaryDamage: 3,
+                    numTargets: 2,
+                    targetArea: 2,
                 },
             ],
         },
