cb39eaea5599d4eb51830b5b5c833ed4d98deb35
Hammerlord <nepharus@gmail.com>
2024-03-28 17:50:38 -0700

    Fade-in animation for summoned/mutated minions

--- src/battle/EndTurnButton.tsx
+++ src/battle/EndTurnButton.tsx
@@ -1,7 +1,9 @@
 import classNames from "classnames";
 import { createUseStyles } from "react-jss";
+import { COMMON_STYLES } from "../constants";
 
 const useStyles = createUseStyles({
+    ...COMMON_STYLES,
     root: {
         fontFamily: "Barlow, Arial",
         padding: "8px 16px",
@@ -12,14 +14,21 @@ const useStyles = createUseStyles({
         border: "2px solid rgba(0, 0, 0, 0.3)",
         boxShadow: "1px 1px 3px rgba(0, 0, 0, 0.5)",
         cursor: "pointer",
+        transition: "0.25s",
+        "&:active": {
+            transform: "translate(1.5px, 1.5px)",
+        },
         "&.highlight": {
             background: "#25b814",
             color: "white",
+            textShadow:
+                "0 0 3px rgba(0, 0, 0, 1), 0 0 3px rgba(0, 0, 0, 1), 0 0 2px rgba(0, 0, 0, 1), 0 0 2px rgba(0, 0, 0, 1), 0 0 2px rgba(0, 0, 0, 1)",
         },
-
         "&.disabled": {
             background: "#cccccc",
             color: "rgba(0, 0, 0, 0.5)",
+            cursor: "auto",
+            textShadow: "none",
         },
     },
 });
@@ -31,6 +40,7 @@ const EndTurnButton = ({ disabled, onClick, highlight }) => {
             className={classNames(classes.root, {
                 highlight,
                 disabled,
+                [classes.highlightAnimation]: highlight && !disabled,
             })}
             disabled={disabled}
             onClick={onClick}
