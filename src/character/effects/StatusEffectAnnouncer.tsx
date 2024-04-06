43adbd7634daa45b392d78380839f125b25c1ccc
Hammerlord <nepharus@gmail.com>
2024-04-05 20:40:35 -0700

    Don't cut off status effect announcement unless there's another status effect being applied/removed right after

--- src/character/effects/StatusEffectAnnouncer.tsx
+++ src/character/effects/StatusEffectAnnouncer.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { Combatant } from "../types";
 import { StatChange } from "../../battle/utils";
 import Icon from "../../icon/Icon";
 import { createUseStyles } from "react-jss";
 import classNames from "classnames";
 
-const PLAYBACK_TIME = 2500;
+const PLAYBACK_TIME = 2000;
 
 const floatAnimation = ({ object, delay, playbackTime = PLAYBACK_TIME }) => {
     const animationFrames: any[] = [
@@ -39,7 +39,7 @@ const useStyles = createUseStyles({
         fontWeight: "bold",
         fontSize: "18px",
         filter: Array.from({ length: 5 })
-            .map(() => "drop-shadow(0 0 2px rgba(255, 255, 255, 0.8))")
+            .map(() => "drop-shadow(0 0 1px rgba(255, 255, 255, 0.8))")
             .join(" "),
         whiteSpace: "nowrap",
     },
@@ -55,15 +55,67 @@ const useStyles = createUseStyles({
 const StatusEffectAnnouncer = ({ statChanges }: { statChanges: StatChange }) => {
     const classes = useStyles();
     const itemRefs = Array.from({ length: 100 }).map(() => useRef());
-    const effects = statChanges?.effects?.filter((e) => e.icon && !e.disableDisplayIcon) || [];
-    const removedEffects = statChanges?.removedEffects?.filter((e) => e.icon && !e.disableDisplayIcon) || [];
+    const [effects, setEffects] = useState([]);
+    const [removedEffects, setRemovedEffects] = useState([]);
+
+    // Only display effects that are visible via icon
+    const newEffects = statChanges?.effects?.filter((e) => e.icon && !e.disableDisplayIcon) || [];
+    const newRemovedEffects = statChanges?.removedEffects?.filter((e) => e.icon && !e.disableDisplayIcon) || [];
+
+    const getStringified = (effects) => {
+        return JSON.stringify(effects.map((e) => e.id));
+    };
+
+    const animatedEffectsRef: any = useRef({});
 
     useEffect(() => {
-        // Only display effects that are visible via icon
-        const animations = [...effects, ...removedEffects].map((e, i) => {
-            return floatAnimation({ object: itemRefs[i].current, delay: i * 100 });
+        if (newEffects.length) {
+            setEffects((prev) => {
+                if (getStringified(prev) === getStringified(newEffects)) {
+                    return prev;
+                }
+
+                return newEffects;
+            });
+        }
+
+        if (newRemovedEffects.length) {
+            setRemovedEffects((prev) => {
+                if (getStringified(prev) === getStringified(newRemovedEffects)) {
+                    return prev;
+                }
+
+                return newRemovedEffects;
+            });
+        }
+    }, [getStringified(newEffects), getStringified(newRemovedEffects)]);
+
+    useEffect(() => {
+        effects.map((e, i) => {
+            const key = e.id;
+            if (!animatedEffectsRef.current[key]) {
+                animatedEffectsRef.current[key] = true;
+                const animation = floatAnimation({ object: itemRefs[i].current, delay: i * 100 });
+                animation.onfinish = () => {
+                    delete animatedEffectsRef.current[key];
+                };
+            }
         });
-    }, [JSON.stringify(statChanges.effects), JSON.stringify(statChanges.removedEffects)]);
+    }, [effects]);
+
+    useEffect(() => {
+        removedEffects.map((e, i) => {
+            i = effects.length + i; // Comes after effects.
+            const key = [e.id, "removed"].join("-");
+            if (!animatedEffectsRef.current[key]) {
+                animatedEffectsRef.current[key] = true;
+                const animation = floatAnimation({ object: itemRefs[i].current, delay: i * 100 });
+                animation.onfinish = () => {
+                    delete animatedEffectsRef.current[key];
+                };
+            }
+        });
+    }, [removedEffects]);
 
     return (
         <div>
