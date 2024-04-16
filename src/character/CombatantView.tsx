196f20e6d6d8540260447f00e194db13e4cfa830
Hammerlord <nepharus@gmail.com>
2024-04-15 19:48:41 -0700

    EffectIcons uses the upcoming Combatant object

--- src/character/CombatantView.tsx
+++ src/character/CombatantView.tsx
@@ -551,7 +551,7 @@ const CombatantView = forwardRef(
                     </div>
                     {(oldState?.HP > 0 || isLifeLinked) && (
                         <div className={classes.effectsContainer}>
-                            <EffectIconsContainer isSilenced={isSilenced} combatant={oldState} event={event} />
+                            <EffectIconsContainer isSilenced={isSilenced} combatant={combatant} event={event} />
                         </div>
                     )}
                 </div>
