a2f44de5634db9cc20cdfab12ab68b23c39b2d02
Hammerlord <nepharus@gmail.com>
2024-04-22 16:03:01 -0700

    Queue all status effect announcements to make it more smooth. Previously was chunky when new effects were applied/removed

--- src/character/effects/StatusEffectAnnouncer.tsx
+++ src/character/effects/StatusEffectAnnouncer.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useMemo, useRef, useState } from "react";
-import { Combatant } from "../types";
+import classNames from "classnames";
+import { useEffect, useRef, useState } from "react";
+import { createUseStyles } from "react-jss";
+import { CombatEffect, EFFECT_TYPES } from "../../ability/types";
 import { StatChange } from "../../battle/utils";
 import Icon from "../../icon/Icon";
-import { createUseStyles } from "react-jss";
-import classNames from "classnames";
-import { EFFECT_TYPES } from "../../ability/types";
+import { Combatant } from "../types";
 
 const PLAYBACK_TIME = 2000;
 
@@ -43,6 +43,7 @@ const useStyles = createUseStyles({
             .map(() => "drop-shadow(0 0 1px rgba(255, 255, 255, 0.8))")
             .join(" "),
         whiteSpace: "nowrap",
+        height: 0,
     },
     faded: {
         color: "#555",
@@ -59,27 +60,23 @@ const useStyles = createUseStyles({
     },
 });
 
+type EffectQueued = {
+    effect: CombatEffect;
+    type: "added" | "removed" | "immuned";
+};
+
 /**
  * A widget for announcing when status effects have been added or have faded
  */
 const StatusEffectAnnouncer = ({ statChanges, combatant }: { statChanges: StatChange; combatant: Combatant }) => {
     const classes = useStyles();
-    const itemRefs = Array.from({ length: 100 }).map(() => useRef());
-    const [effects, setEffects] = useState([]);
-    const [removedEffects, setRemovedEffects] = useState([]);
-
-    // Only display effects that are visible via icon
-    const newEffects = statChanges?.effects?.filter((e) => e.icon && !e.disableDisplayIcon) || [];
-    const newRemovedEffects = statChanges?.removedEffects?.filter((e) => e.icon && !e.disableDisplayIcon) || [];
-
-    const getStringified = (effects) => {
-        return JSON.stringify(effects.map((e) => e.id));
-    };
+    const ref = useRef({});
+    const [queue, setQueue]: [EffectQueued[], Function] = useState([]);
 
     const isInvalidCombatant =
         !combatant || (combatant.HP === 0 && combatant.effects.every((effect) => effect.type !== EFFECT_TYPES.LIFE_LINK));
 
-    const aggregate = (effects) => {
+    const aggregate = (effects): CombatEffect[] => {
         const aggregated = effects.reduce((acc, effect) => {
             if (!acc[effect.name]) {
                 acc[effect.name] = {
@@ -98,84 +95,130 @@ const StatusEffectAnnouncer = ({ statChanges, combatant }: { statChanges: StatCh
 
     const animatedEffectsRef: any = useRef({});
 
+    const getKey = (effect, type) => {
+        return [effect.id, type].join("-");
+    };
+
+    useEffect(() => {
+        if (isInvalidCombatant || !statChanges) {
+            return;
+        }
+
+        // Only display effects that are visible via icon
+        const { effects = [], removedEffects = [] } = statChanges;
+        const newEffects = effects.filter((e) => e.icon && !e.disableDisplayIcon);
+        const newRemovedEffects = removedEffects.filter((e) => e.icon && !e.disableDisplayIcon);
+
+        const queuedKeys = queue.reduce((acc, item) => {
+            acc[getKey(item.effect, item.type)] = true;
+            return acc;
+        }, {});
+
+        const isAlreadyQueued = (effect: CombatEffect, type): boolean => {
+            const key = getKey(effect, type);
+            return Boolean(queuedKeys[key] || animatedEffectsRef[key]);
+        };
+
+        const newQueue = [...queue];
+        aggregate(newEffects).forEach((effect) => {
+            if (!isAlreadyQueued(effect, "added")) {
+                newQueue.push({ effect, type: "added" });
+            }
+        });
+
+        aggregate(newRemovedEffects).forEach((effect) => {
+            if (!isAlreadyQueued(effect, "removed")) {
+                newQueue.push({ effect, type: "removed" });
+            }
+        });
+
+        if (queue.length === newQueue.length) {
+            return;
+        }
+
+        setQueue(newQueue);
+    }, [statChanges]);
+
     useEffect(() => {
         if (isInvalidCombatant) {
             return;
         }
-        if (newEffects.length) {
-            setEffects((prev) => {
-                const aggregated = aggregate(newEffects);
-                if (getStringified(prev) === getStringified(aggregated)) {
-                    return prev;
-                }
+        queue.forEach((item, i) => {
+            const { effect: e, type } = item;
+            const key = getKey(e, type);
 
-                return aggregated;
-            });
-        }
-
-        if (newRemovedEffects.length) {
-            setRemovedEffects((prev) => {
-                const aggregated = aggregate(newRemovedEffects);
-
-                if (getStringified(prev) === getStringified(aggregated)) {
-                    return prev;
-                }
-
-                return aggregated;
-            });
-        }
-    }, [newEffects, newRemovedEffects]);
-
-    useEffect(() => {
-        if (isInvalidCombatant) {
-            return;
-        }
-        effects.map((e, i) => {
-            const key = e.id;
+            const elementRef = ref.current?.[key];
+            if (!elementRef) {
+                return;
+            }
             if (!animatedEffectsRef.current[key]) {
                 animatedEffectsRef.current[key] = true;
-                const animation = floatAnimation({ object: itemRefs[i].current, delay: i * 100 });
+                const animation = floatAnimation({ object: elementRef, delay: i * 250 });
                 animation.onfinish = () => {
                     delete animatedEffectsRef.current[key];
+                    setQueue((prev) =>
+                        prev.filter((item) => {
+                            const queueItemKey = getKey(item.effect, item.type);
+                            return queueItemKey !== key;
+                        })
+                    );
                 };
             }
         });
-    }, [effects]);
-
-    useEffect(() => {
-        if (isInvalidCombatant) {
-            return;
-        }
-        removedEffects.map((e, i) => {
-            i = effects.length + i; // Comes after effects.
-            const key = [e.id, "removed"].join("-");
-            if (!animatedEffectsRef.current[key]) {
-                animatedEffectsRef.current[key] = true;
-                const animation = floatAnimation({ object: itemRefs[i].current, delay: i * 100 });
-                animation.onfinish = () => {
-                    delete animatedEffectsRef.current[key];
-                };
-            }
-        });
-    }, [removedEffects]);
+    }, [queue]);
 
     return (
-        <div>
-            <div className={classes.effectClassContainer}>
-                {effects.map((e, i) => (
-                    <div key={e.id} className={classes.effectItem} ref={itemRefs[i]}>
-                        + <Icon icon={e.icon} size="sm" /> {e.name} {e.stacks > 1 ? `x${e.stacks}` : undefined}
+        <div className={classes.effectClassContainer}>
+            {queue.map((item) => {
+                const { effect: e, type } = item;
+
+                const getInner = () => {
+                    if (type === "added") {
+                        return (
+                            <>
+                                + <Icon icon={e.icon} size="sm" /> {e.name} {e.stacks > 1 ? `x${e.stacks}` : undefined}
+                            </>
+                        );
+                    }
+
+                    if (type === "removed") {
+                        return (
+                            <>
+                                {" "}
+                                <Icon icon={e.icon} size="sm" className={classes.fadedIcon} /> {e.name}{" "}
+                                {e.stacks > 1 ? `x${e.stacks}` : undefined} faded
+                            </>
+                        );
+                    }
+
+                    if (type === "immuned") {
+                        return (
+                            <>
+                                Resisted <Icon icon={e.icon} size="sm" className={classes.fadedIcon} /> {e.name}
+                            </>
+                        );
+                    }
+                };
+
+                const key = getKey(e, type);
+                return (
+                    <div
+                        key={key}
+                        className={classNames(classes.effectItem, {
+                            [classes.faded]: type === "removed" || type === "immuned",
+                        })}
+                        ref={(element) => {
+                            if (element) {
+                                ref.current[key] = element;
+                            } else {
+                                delete ref.current[key];
+                            }
+                        }}
+                    >
+                        {getInner()}
                     </div>
-                ))}
-            </div>
-            <div className={classes.effectClassContainer}>
-                {removedEffects.map((e, i) => (
-                    <div key={e.id} className={classNames(classes.effectItem, classes.faded)} ref={itemRefs[effects.length + i]}>
-                        <Icon icon={e.icon} size="sm" className={classes.fadedIcon} /> {e.name} {e.stacks > 1 ? `x${e.stacks}` : undefined}{" "}
-                        faded
-                    </div>
-                ))}
-            </div>
+                );
+            })}
         </div>
     );
 };
