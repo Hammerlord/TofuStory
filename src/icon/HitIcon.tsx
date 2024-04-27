190f636959c82d684a9c2abcfd706b57a691685d
Hammerlord <nepharus@gmail.com>
2024-04-26 22:08:53 -0700

    Hit 'boom' is a bit more dynamic

--- src/icon/HitIcon.tsx
+++ src/icon/HitIcon.tsx
@@ -2,6 +2,7 @@ import { MutableRefObject, useEffect, useRef, useState } from "react";
 import { createUseStyles } from "react-jss";
 import { BoomImage } from "../images";
 import Icon from "./Icon";
+import { getRandomInt } from "../utils";
 
 const useStyles = createUseStyles({
     root: {
@@ -11,60 +12,91 @@ const useStyles = createUseStyles({
         transform: "translateX(-50%) translateY(-50%)",
         display: "none",
         filter: "drop-shadow(0px 0px 1px rgba(0, 0, 0, 1)) drop-shadow(1px 1px 1px rgba(0, 0, 0, 0.8))",
-        "& .icon": {
-            width: "225%",
-            height: "225%",
-            position: "absolute",
-            top: "45%",
-            left: "42%",
-            transform: "translateX(-50%) translateY(-50%)",
-        },
-        "& .text": {
-            position: "absolute",
-            top: "40%",
-            left: "50%",
-            transform: "translateX(-50%) translateY(-50%)",
-            fontSize: "22px",
-            textShadow: Array.from({ length: 10 })
-                .map(() => "0 0 3px black")
-                .join(", "),
-        },
+        width: "30px",
+        height: "30px",
+        minWidth: "30px",
+        minHeight: "30px",
+    },
+    icon: {
+        width: "225%",
+        height: "225%",
+        position: "absolute",
+        top: "45%",
+        left: "42%",
+        transform: "translateX(-50%) translateY(-50%)",
+        margin: "auto",
+    },
+    text: {
+        position: "absolute",
+        color: "white",
+        fontWeight: "bold",
+        top: "40%",
+        left: "50%",
+        transform: "translateX(-50%) translateY(-50%)",
+        fontSize: "22px",
+        textShadow: Array.from({ length: 10 })
+            .map(() => "0 0 3px black")
+            .join(", "),
     },
 });
 
 const HitIcon = ({ statChanges }) => {
     const [oldStatChanges, setOldStatChanges] = useState({ damage: 0 });
+    const [pos, setPos] = useState({ x: 50, y: 50 });
     const classes = useStyles();
-    const ref: MutableRefObject<HTMLSpanElement> = useRef();
-    const animationRef: MutableRefObject<Animation> = useRef();
+    const rootRef: MutableRefObject<HTMLSpanElement> = useRef();
+    const iconRef: MutableRefObject<HTMLImageElement> = useRef();
+    const animationRefs: MutableRefObject<any> = useRef([]);
 
     useEffect(() => {
         if (!statChanges.damage) {
             return;
         }
 
-        if (ref.current) {
-            animationRef.current?.cancel();
-            const animation = ref.current?.animate(
+        if (rootRef.current) {
+            animationRefs.current?.forEach((animation) => animation.cancel());
+            animationRefs.current = [];
+            const rootAnimation = rootRef.current.animate(
                 [
                     {
                         opacity: 1,
                         offset: 0.8,
                         display: "block",
                     },
+
                     { opacity: 0, display: "block" },
                 ],
                 1500
             );
 
-            animationRef.current = animation;
+            animationRefs.current.push(rootAnimation);
+
+            const iconAnimation = iconRef.current.animate(
+                [
+                    {
+                        scale: 0.5,
+                        transform: "translateX(-75%) translateY(-75%)",
+                    },
+                    {
+                        scale: 1,
+                    },
+                ],
+                {
+                    duration: 150,
+                    fill: "forwards",
+                }
+            );
+            animationRefs.current.push(iconAnimation);
+
+            setPos({ x: getRandomInt(30, 70), y: getRandomInt(30, 70) });
             setOldStatChanges(statChanges);
         }
     }, [statChanges]);
 
     return (
-        <span className={classes.root} ref={ref}>
-            <Icon icon={BoomImage} text={oldStatChanges.damage} />
+        <span className={classes.root} ref={rootRef} style={{ transform: `translateX(-${pos.x}%) translateY(-${pos.y}%)` }}>
+            <img src={BoomImage} className={classes.icon} ref={iconRef} />
+            <span className={classes.text}>{oldStatChanges.damage}</span>
         </span>
     );
 };
