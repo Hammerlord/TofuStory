e40071bc440b171234c3cb6c41c84d016087b9e3
Hammerlord <nepharus@gmail.com>
2026-08-25 18:11:39 -0700

    Fix import

--- src/devtools/DevStageBattle.tsx
+++ src/devtools/DevStageBattle.tsx
@@ -1,19 +1,13 @@
 import { useEffect, useMemo } from "react";
 
-import { bowmanProperties, wizardProperties } from "../character/defaultCharacterProperties";
-import { playerStateSlice } from "../character/playerReducer";
-import { devDummy, spikedDummy } from "../enemy/dummy";
-import { faust, ghostlyPuppeteerL, ghostlyPuppeteerR } from "../enemy/faust";
-import { useAppDispatch, useAppSelector } from "../hooks";
-import { deathLaser } from "./deathLaser";
-import { bladedArmor, block, closeCombat, divineCharge, dustDevils, hammerang, shieldCharge } from "../ability/warrior/warriorAbilities";
-import { goutOfFlame, icyDraft, lesserBolt, whelp, zap } from "../ability/magician/magicianAbilities";
-import { manji } from "../enemy/Manji";
-import { gachaponMachine } from "../scene/gachapon/Gachapon";
-import { entrenchedFire, guard, shatteringArrow, windupShot } from "../ability/bowman/bowmanAbilities";
-import { curseEye } from "../enemy/enemy";
+import { entrenchedFire, guard, shatteringArrow } from "../ability/bowman/bowmanAbilities";
+import { bladedArmor, closeCombat, dustDevils } from "../ability/warrior/warriorAbilities";
 import { startBattle } from "../battle/actions/phases/phases";
 import BattlefieldContainer from "../battle/view/BattleView";
+import { bowmanProperties } from "../character/defaultCharacterProperties";
+import { playerStateSlice } from "../character/playerReducer";
+import { devDummy, spikedDummy } from "../enemy/dummy";
+import { useAppDispatch, useAppSelector } from "../hooks";
 
 const { updatePlayer, updateDeck } = playerStateSlice?.actions || {};
 const dummies = [devDummy, devDummy, devDummy, devDummy, devDummy];
