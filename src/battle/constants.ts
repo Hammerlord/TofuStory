0df00105f4549f80899625d8c2e3062ba42479b7
Hammerlord <nepharus@gmail.com>
2026-09-09 18:25:35 -0700

    Dunno why scaling DoT damage came back, all of them should do 1 damage per stack

--- src/battle/constants.ts
+++ src/battle/constants.ts
@@ -38,8 +38,8 @@ export const battleWarnings = {
 
 export const dotDamageMap: Partial<Record<EFFECT_TYPES, number>> = {
     [EFFECT_TYPES.BLEED]: 1,
-    [EFFECT_TYPES.POISON]: 2,
-    [EFFECT_TYPES.BURN]: 3,
+    [EFFECT_TYPES.POISON]: 1,
+    [EFFECT_TYPES.BURN]: 1,
 };
 
 export const dotAbilityMap = {
