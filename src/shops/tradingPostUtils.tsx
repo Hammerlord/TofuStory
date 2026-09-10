42006fc3d6aafa28015479f6259f9391ec94101f
Hammerlord <nepharus@gmail.com>
2026-09-10 13:40:12 -0700

    Since Trading Post always offers mesos, do not roll meso items separately for it

--- src/shops/tradingPostUtils.tsx
+++ src/shops/tradingPostUtils.tsx
@@ -9,10 +9,15 @@ import { NUM_TRADING_POST_ITEMS } from "./constants";
 export const generateTradingPostInventory = (player: Player) => {
     const upgradedStarterItem = STARTER_ITEM_UPGRADE_MAP[player.class];
 
+    const mesoItems = [mesoItem, bigMesoItem, hugeMesoItem];
     const itemPool = shuffle(
-        filterUnobtainableItems({ playerItems: player.items, itemsToFilter: ITEMS.concat(CLASS_ITEMS[player.class] || []) })
+        filterUnobtainableItems({
+            playerItems: player.items,
+            excludeItems: mesoItems,
+            itemsToFilter: ITEMS.concat(CLASS_ITEMS[player.class] || []),
+        })
     );
-    const items = itemPool.slice(0, NUM_TRADING_POST_ITEMS).concat([mesoItem, bigMesoItem, hugeMesoItem]);
+    const items = itemPool.slice(0, NUM_TRADING_POST_ITEMS).concat(mesoItems);
 
     if (upgradedStarterItem && !player.items.some((item) => item.name === upgradedStarterItem.name)) {
         items.push(upgradedStarterItem);
