689888e992d0035fed4d678d9556986f1982270d
Hammerlord <nepharus@gmail.com>
2026-09-20 13:03:45 -0700

    Set the test env to jsdom for React component tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 // https://vitest.dev/config/
 export default defineConfig({
     test: {
-        environment: "node",
+        environment: "jsdom",
         setupFiles: ["./src/test/setup.ts"],
         include: ["src/**/__tests__/**/*.{test,spec}.{ts,tsx}"],
     },
