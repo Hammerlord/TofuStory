d856618c860fae08b1ce44a7bd7beb361af0e63c
Hammerlord <nepharus@gmail.com>
2026-09-24 19:05:50 -0700

    Fix loading SVGs in the test env

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vitest/config";
+import svgr from "vite-plugin-svgr";
 
 // https://vitest.dev/config/
 export default defineConfig({
+    plugins: [
+        svgr({
+            include: "**/*.svg",
+            exclude: [/\?raw/],
+            svgrOptions: {
+                exportType: "default",
+            },
+        }),
+    ],
     test: {
         environment: "jsdom",
         setupFiles: ["./src/test/setup.ts"],
